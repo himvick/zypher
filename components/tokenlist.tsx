432ac06fc2775d25749bccf3fd9455c9610a5cb0
Victor <grantv076@gmail.com>
2026-01-28 14:45:28 -0800

    added getting token list data functionality

--- components/tokenlist.tsx
+++ components/tokenlist.tsx
@@ -10,6 +10,7 @@ interface Props {
 const TokenList = ({ address }: Props) => {
   useEffect(() => {
     const fetchTokens = async () => {
+      let allTokens = [];
       try {
         const res = await fetch(ALCHEMY_API_URL, {
           method: "POST",
@@ -23,12 +24,30 @@ const TokenList = ({ address }: Props) => {
         });
         const data = await res.json();
         let tokenAddresses = data.result.tokenBalances
-          .filter((token: any) => token.tokenBalance != 0)
+          .filter((token: any) => token.tokenBalance == 0)
           .map((token: any) => token.contractAddress);
-        console.log(tokenAddresses);
-      } catch (error) {}
+        for (let i = 0; i < tokenAddresses.length; i++) {
+          const result = await fetch(ALCHEMY_API_URL, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              jsonrpc: "2.0",
+              id: 1,
+              method: "alchemy_getTokenMetadata",
+              params: [tokenAddresses[i]],
+            }),
+          });
+          const dat = await result.json();
+          allTokens.push({ ...dat.result, address: tokenAddresses[i] });
+        }
+        localStorage.setItem("allTokens", JSON.stringify(allTokens));
+      } catch (error) {
+        console.error("Error getting tokens: ", error);
+      }
     };
-    fetchTokens();
+    if (JSON.parse(localStorage.getItem("allTokens") as string)?.length == 0) {
+      fetchTokens();
+    }
   }, []);
   return <div></div>;
 };
