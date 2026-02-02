04596f48b91e32c4ad0c6e2ebdde3a94810103a1
Victor <grantv076@gmail.com>
2026-02-02 11:20:10 -0800

    added mechanism for fetching wallet information in buy page

--- app/home/buy/page.tsx
+++ app/home/buy/page.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import { HDNodeWallet, Wallet } from "ethers";
+import React, { useEffect, useState } from "react";
 
 const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+  const [wallet, setWallet] = useState<HDNodeWallet>();
+  const [amount, setAmount] = useState<string>("0.00");
+  useEffect(() => {
+    const password = localStorage.getItem("password");
+    const storedEncryptedWallet = localStorage.getItem("encryptedWallet");
+    if (password && storedEncryptedWallet) {
+      Wallet.fromEncryptedJson(storedEncryptedWallet, password).then(
+        (wallet) => {
+          setWallet(wallet as HDNodeWallet);
+        },
+      );
+    }
+  });
 
-export default page
+  const setInput = () => {};
+
+  return (
+    <div>
+      <div>
+        <h2>Buy</h2>
+        <hr />
+        <div>
+          <p>
+            {wallet?.address.slice(0, 7) + "..." + wallet?.address.slice(-4)}
+          </p>
+        </div>
+        <div>
+          <h1>GHS</h1>
+          <input type="text" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
--- components/menubutton.tsx
+++ components/menubutton.tsx
@@ -41,3 +41,5 @@ const MenuButton = () => {
 };
 
 export default MenuButton;
+
+
