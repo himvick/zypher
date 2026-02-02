f8090f5432c24673041ba9f548bd7e2283a89f49
Victor <grantv076@gmail.com>
2026-02-02 15:19:10 -0800

    styled receive page

--- app/home/receive/page.tsx
+++ app/home/receive/page.tsx
@@ -22,31 +22,36 @@ const page = () => {
     navigator.clipboard.writeText(data);
   };
   return (
-    <div>
-      <div>
-        <div>
-          <p>{wallet?.address}</p>
+    <div className="flex flex-col justify-center items-center gap-2 h-lvh">
+      <h2 className="text-3xl">Receive</h2>
+      <div className="border rounded-3xl flex flex-col p-4 gap-4 items-center">
+        <div className="rounded-2xl p-2 border">
+          <div>
+            <p>
+              {wallet?.address.slice(0, 7) + "..." + wallet?.address.slice(-4)}
+            </p>
+          </div>
         </div>
-      </div>
-      <div>
+        <div>
+          {wallet && (
+            <QRCodeCanvas
+              value={wallet.address}
+              size={300}
+              bgColor="white"
+              fgColor="black"
+              level="H"
+            />
+          )}
+        </div>
         {wallet && (
-          <QRCodeCanvas
-            value={wallet.address}
-            size={200}
-            bgColor="white"
-            fgColor="black"
-            level="H"
-          />
+          <button
+            className="rounded-2xl bg-blue-950 px-4 py-2 cursor-pointer text-white"
+            onClick={() => copyAddress(wallet.address)}
+          >
+            {buttonText}
+          </button>
         )}
       </div>
-      {wallet && (
-        <button
-          className="rounded-2xl bg-blue-950 px-4 py-2 cursor-pointer text-white"
-          onClick={() => copyAddress(wallet.address)}
-        >
-          {buttonText}
-        </button>
-      )}
     </div>
   );
 };
