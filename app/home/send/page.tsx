0b0e3498c9d981a329d52685bad68384286a717e
Victor <grantv076@gmail.com>
2026-02-02 15:44:23 -0800

    added html elements for send page

--- app/home/send/page.tsx
+++ app/home/send/page.tsx
@@ -1,9 +1,64 @@
-import React from 'react'
+"use client";
+import { HDNodeWallet, Wallet } from "ethers";
+import React, { useEffect, useState } from "react";
 
 const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+  const [wallet, setWallet] = useState<HDNodeWallet>();
+  const [toAddress, setToAddress] = useState<string>("");
+  const [amount, setAmount] = useState<string>("");
+  useEffect(() => {
+    const password = localStorage.getItem("password");
+    const storedEncryptedWallet = localStorage.getItem("encryptedWallet");
+    if (password && storedEncryptedWallet) {
+      Wallet.fromEncryptedJson(storedEncryptedWallet, password).then(
+        (wallet) => {
+          setWallet(wallet as HDNodeWallet);
+        },
+      );
+    }
+  }, []);
 
-export default page
+  return (
+    <div>
+      <h2>Send</h2>
+      <div>
+        <div>
+          <p>From</p>
+          <div>
+            <p>
+              {wallet?.address.slice(0, 7) + "..." + wallet?.address.slice(-4)}
+            </p>
+          </div>
+        </div>
+        <div>
+          <p>To</p>
+          <input
+            type="text"
+            value={toAddress}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setToAddress(e.target.value)
+            }
+          />
+        </div>
+        <div>
+          <p>Amount</p>
+          <input
+            type="number"
+            value={amount}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setAmount(e.target.value)
+            }
+          />
+        </div>
+        <div>Ethereum</div>
+        <div>
+          <button className="rounded-2xl bg-blue-950 text-white px-4 py-2">
+            Send
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
